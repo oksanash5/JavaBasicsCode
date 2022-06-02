abfcbe0c5ff85f977a820c59f92d2d75fb00ead2
oksanash5 <oksanash55@gmail.com>
2022-06-01 21:31:16 -0400

    modifed the git class04

--- src/com/syntax/gitclass/gitClass04.java
+++ src/com/syntax/gitclass/gitClass04.java
@@ -4,7 +4,8 @@ public class gitClass04 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       System.out.println("popculture");	
+       System.out.println("popculture");
+       System.out.println("abrakedabra");
        }
 
 }
